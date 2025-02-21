f5b0fc8c0f5d6a7eaadd11ff8ba13d2d3020049e
nguyenduy <nguyenduy011201@gmail.com>
2025-02-21 16:52:14 +0700

    first commit

---
+++ src/app/main/page/auth/auth.module.ts
@@ -0,0 +1,10 @@
+// angular import
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CRouterModule } from './main-router.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, CRouterModule]
+})
+export class MainModule {}
